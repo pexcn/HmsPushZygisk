6b16c66f4dbcc3fa5e27f06b5e5f4baadf9268fb
fei-ke <jinyang656@gmail.com>
2026-03-29 00:31:16 +0800

    style: correct window insets

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { enableEdgeToEdge } from 'kernelsu'
 import './style.css'
-
-enableEdgeToEdge(true);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
